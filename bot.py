81fa976ea2b71dacd2fd59babd76079d0041704a
Ahmet-Furkan-Karatas <ahmet_karatas44@hotmail.com>
2024-12-16 23:21:03 +0300

    fıtı fıtı

--- bot.py
+++ bot.py
@@ -11,6 +11,12 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'Giriş yapıldı:  {bot.user.name}')
+    
+@bot.event
+async def on_member_join(member):
+    # Karşılama mesajı gönderme
+    for channel in member.guild.text_channels:
+        await channel.send(f'Hoş geldiniz: , {member.mention}!')
 
 @bot.command()
 async def start(ctx):
@@ -20,11 +26,8 @@ async def start(ctx):
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member: discord.Member = None):
     if member:
-        if ctx.author.top_role <= member.top_role:
-            await ctx.send("Eşit veya daha yüksek rütbeli bir kullanıcıyı banlamak mümkün değildir!")
-        else:
-            await ctx.guild.ban(member)
-            await ctx.send(f"Kullanızı {member.name} banlandı")
+        await ctx.guild.ban(member)
+        await ctx.send(f"Kullanıcı {member.name} banlandı")
     else:
         await ctx.send("Bu komut banlamak istediğiniz kullanıcıyı işaret etmelidir. Örneğin: `!ban @user`")
 
@@ -34,5 +37,16 @@ async def ban_error(ctx, error):
         await ctx.send("Bu komutu çalıştırmak için yeterli izniniz yok.")
     elif isinstance(error, commands.MemberNotFound):
         await ctx.send("Kullanıcı bulunamadı!")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if "https://" in message.content:
+        await message.guild.ban(message.author)
+        await message.channel.send(f"{message.author.name} bağlantı gönderdiği için banlandı.")
+    else:
+        await bot.process_commands(message)
 
 bot.run(token)
